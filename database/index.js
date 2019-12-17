8774b17dae28fbc8c0bfe4c9c7d61d6ddfcc8232
andrepp97 <andreputerap@gmail.com>
2019-12-17 15:37:16 +0700

    change db host

--- database/index.js
+++ database/index.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql')
 
 const conn = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'tonton',
+    host: 'remotemysql.com',
+    user: 'GKIdGIS9IU',
+    password: 'T6C5dbOAXG',
+    database: 'GKIdGIS9IU',
     timezone: 'UTC'
 })
 
